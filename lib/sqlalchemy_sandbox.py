34fd37fc8618511409a15fbae438e94fae11321d
KELVIN MURITHI <kelvinmurithi@live.com>
2023-12-05 22:25:20 +0300

    created records

--- lib/sqlalchemy_sandbox.py
+++ lib/sqlalchemy_sandbox.py
@@ -12,14 +12,43 @@ Base = declarative_base()
 class Student(Base):
     __tablename__ = 'students'
 
+    Index('index_name', 'name')
+
     id = Column(Integer(), primary_key=True)
     name = Column(String())
+    email = Column(String(55))
+    grade = Column(Integer())
+    birthday = Column(DateTime())
+    enrolled_date = Column(DateTime(), default=datetime.now())
+
+    def __repr__(self):
+        return f"Student {self.id}: " \
+            + f"{self.name}, " \
+            + f"Grade {self.grade}"
+
+# imports, models
 
 if __name__ == '__main__':
+
     engine = create_engine('sqlite:///:memory:')
     Base.metadata.create_all(engine)
 
-    # use our engine to configure a 'Session' class
     Session = sessionmaker(bind=engine)
-    # use 'Session' class to create 'session' object
     session = Session()
+
+    albert_einstein = Student(
+        name="Albert Einstein",
+        email="albert.einstein@zurich.edu",
+        grade=6,
+        birthday=datetime(
+            year=1879,
+            month=3,
+            day=14
+        ),
+    )
+
+    session.add(albert_einstein)
+    session.commit()
+
+    print(f"New student ID is {albert_einstein.id}.")
+
